af360863d23383687eaa42d8fe834a93759c3bb2
dattadeepayan <44273830+dattadeepayan@users.noreply.github.com>
2021-12-10 17:53:52 +0000

    Delete runme.sh

--- runme.sh
+++
@@ -1,8 +0,0 @@
-#Command to run the Hub
-python3 hub_n.py --port 34000 --deviceIP 10.35.70.37 &
-
-#Command to run the Ambulance
-#python3 ambulance.py --deviceID 125 --port 34000 --hubIP 10.35.70.37 &
-
-#Command to run the Body Area Network
-#python3 body_data.py --deviceID Evelyn --port 34000 --mode emergency --emport 33090 --hubIP 10.35.70.37 --patientIP 10.35.70.37 &
